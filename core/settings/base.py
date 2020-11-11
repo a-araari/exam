716b6a71b7aab3e52129742f7039e6a3f06ca9d3
A-Arari <araariadem0@gmail.com>
2020-11-11 13:09:53 +0100

    add scrape app

--- core/settings/base.py
+++ core/settings/base.py
@@ -23,6 +23,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent.parent
 # Application definition
 
 INSTALLED_APPS = [
+    'scrape',
+
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
