8bce06670db5f5ed5f3b81701c7ed373b0db40e4
A-Arari <araariadem0@gmail.com>
2020-11-11 18:36:16 +0100

    Add crawling over all links

--- scrape/management/commands/scrapedevoiratdata.py
+++ scrape/management/commands/scrapedevoiratdata.py
@@ -6,7 +6,7 @@ from bs4 import BeautifulSoup
 from PyPDF2 import PdfFileReader
 
 from django.core.management.base import BaseCommand
-
+from .devoirat_main_urls import URLS
 
 devoirat_url = 'https://www.devoirat.net/'
 pdf_link_re = r'^.+\.([pP][dD][fF])(.*)$'
@@ -22,10 +22,13 @@ class Command(BaseCommand):
     """
 
     def handle(self, *args, **kwargs):
-        url = requests.get(devoirat_url + 'français/devoirs-français/1ère-année/')
-        soup = BeautifulSoup(url.content, 'html.parser')
+        for url in URLS:
+            print(f'crawling {url}:')
 
-        self.extract_pdfs(soup)
+            response = requests.get(url)
+            soup = BeautifulSoup(response.content, 'html.parser')
+
+            self.extract_pdfs(soup)
 
     def extract_pdfs(self, soup):
         """
@@ -41,7 +44,6 @@ class Command(BaseCommand):
                 response = requests.get(pdf_href)
 
                 print ("url match: ", pdf_href)
-                input()
 
                 parent_div = a.parent.parent
 
@@ -54,23 +56,23 @@ class Command(BaseCommand):
 
                 print ("File Data:")
 
-                print("%-15s%-15s%-15s" % ('', 'title:', title.contents[0] if title else None))
+                # print("%-15s%-15s%-15s" % ('', 'title:', title.contents[0] if title else None))
                 print("%-15s%-15s%-15s" % ('', 'file name:', file_name.contents[0] if file_name else None))
-                print("%-15s%-15s%-15s" % ('', 'description:', description.contents[0] if description else None))
-                print("%-15s%-15s%-15s" % ('', 'file type:', file_type.contents[0] if file_type else None))
+                # print("%-15s%-15s%-15s" % ('', 'description:', description.contents[0] if description else None))
+                # print("%-15s%-15s%-15s" % ('', 'file type:', file_type.contents[0] if file_type else None))
                 print("%-15s%-15s%-15s" % ('', 'file size:', file_size.contents[0] if file_size else None))
 
                 with io.BytesIO(response.content) as f:
                     # pdf = PdfFileReader(f)
 
                     pdf = pikepdf.open(f)
-                    print()
+                    # print()
                     print("%-15s%-15s%-15s" % ('', 'pages count', len(pdf.pages)))
 
                     meta_data = pdf.open_metadata()
-                    print("Meta Data:")
-                    print("%-15s%-15s%-15s" % ('', 'dc:creator', meta_data.get('dc:creator')))
-                    print("%-15s%-15s%-15s" % ('', 'dc:title', meta_data.get('dc:title')))
-                    print("%-15s%-15s%-15s" % ('', 'dc:description', meta_data.get('dc:description')))
-                    print("%-15s%-15s%-15s" % ('', 'dc:subject', meta_data.get('dc:subject')))
+                    # print("Meta Data:")
+                    # print("%-15s%-15s%-15s" % ('', 'dc:creator', meta_data.get('dc:creator')))
+                    # print("%-15s%-15s%-15s" % ('', 'dc:title', meta_data.get('dc:title')))
+                    # print("%-15s%-15s%-15s" % ('', 'dc:description', meta_data.get('dc:description')))
+                    # print("%-15s%-15s%-15s" % ('', 'dc:subject', meta_data.get('dc:subject')))
                     print()
