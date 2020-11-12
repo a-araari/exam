cfafec00456b4423e4e0d9ce083e48081945007b
A-Arari <araariadem0@gmail.com>
2020-11-12 14:24:15 +0100

    Add static config

--- core/settings/base.py
+++ core/settings/base.py
@@ -82,4 +82,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
+STATIC_ROOT = 'static_files/'
 STATIC_URL = '/static/'
+
+MEDIA_ROOT = 'media/'
+MEDIA_ROOT = '/media/'
