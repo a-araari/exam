31f1da64e07cea9dd1d5b650cdd4f80111a9414d
A-Arari <araariadem0@gmail.com>
2020-11-12 19:33:19 +0100

    add django_cleanup to installed apps

--- core/settings/base.py
+++ core/settings/base.py
@@ -32,6 +32,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'django_cleanup.apps.CleanupConfig',
 ]
 
 MIDDLEWARE = [
