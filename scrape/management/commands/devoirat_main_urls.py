0094ea179b1cfdf10d50370919082e6396895b9c
A-Arari <araariadem0@gmail.com>
2020-11-12 16:58:46 +0100

    Add new fields to be saved

--- scrape/management/commands/devoirat_main_urls.py
+++ scrape/management/commands/devoirat_main_urls.py
@@ -1,6 +1,5 @@
 URLS = [
     'https://www.devoirat.net/allemand/devoirs-allemand/4ème-année-bac/',
-    'https://www.devoirat.net/anglais/cours-anglais/',
     'https://www.devoirat.net/anglais/cours-anglais/2ème-année/',
     'https://www.devoirat.net/anglais/devoirs-anglais/2ème-année/',
     'https://www.devoirat.net/arabe/devoirs-arabe/1ère-année/',
@@ -10,52 +9,36 @@ URLS = [
     'https://www.devoirat.net/français/devoirs-français/3ème-année/',
     'https://www.devoirat.net/français/séries-français/1ère-année/',
     'https://www.devoirat.net/français/séries-français/2ème-année/',
-    'https://www.devoirat.net/histoire-g%C3%A9ographie/',
-    'https://www.devoirat.net/histoire-géographie/',
     'https://www.devoirat.net/informatique/cours-informatique/bac-eco-gest/',
-    'https://www.devoirat.net/informatique/devoirs-informatique/3ème-année/',
     'https://www.devoirat.net/informatique/devoirs-informatique/bac-sciences-exp/',
     'https://www.devoirat.net/informatique/devoirs-informatique/base-de-donnees/',
     'https://www.devoirat.net/informatique/devoirs-informatique/informatique-1/',
     'https://www.devoirat.net/informatique/devoirs-informatique/technique/',
-    'https://www.devoirat.net/informatique/séries-informatique/2ème-année/',
-    'https://www.devoirat.net/informatique/séries-informatique/3ème-année/',
-    'https://www.devoirat.net/maths/cours-maths/2ème-année/',
     'https://www.devoirat.net/maths/cours-maths/eco-service/',
     'https://www.devoirat.net/maths/cours-maths/maths/',
-    'https://www.devoirat.net/maths/cours-maths/science/',
     'https://www.devoirat.net/maths/cours-maths/sciences/',
-    'https://www.devoirat.net/maths/devoirs-maths/',
+    'https://www.devoirat.net/maths/cours-maths/science/',
     'https://www.devoirat.net/maths/devoirs-maths/2ème-trimestre-3/',
-    'https://www.devoirat.net/maths/devoirs-maths/3ème-année/',
     'https://www.devoirat.net/maths/devoirs-maths/bac-info/',
     'https://www.devoirat.net/maths/devoirs-maths/bac-math/',
     'https://www.devoirat.net/maths/devoirs-maths/bac-sciences-exp/',
     'https://www.devoirat.net/maths/devoirs-maths/maths/',
     'https://www.devoirat.net/maths/devoirs-maths/sciences-1/',
-    'https://www.devoirat.net/maths/séries-maths/',
     'https://www.devoirat.net/maths/séries-maths/1ère-année/',
     'https://www.devoirat.net/maths/séries-maths/2013-2014-2015/',
     'https://www.devoirat.net/maths/séries-maths/2015-2016/',
     'https://www.devoirat.net/maths/séries-maths/2018-2019-2020-1/',
-    'https://www.devoirat.net/maths/séries-maths/2ème-année/',
-    'https://www.devoirat.net/maths/séries-maths/4ème-année-bac/',
     'https://www.devoirat.net/maths/séries-maths/bac-info/',
     'https://www.devoirat.net/maths/séries-maths/eco-services/',
     'https://www.devoirat.net/maths/séries-maths/info/',
     'https://www.devoirat.net/maths/séries-maths/sc-exp/',
     'https://www.devoirat.net/maths/séries-maths/sciences/',
     'https://www.devoirat.net/maths/séries-maths/technique/',
-    'https://www.devoirat.net/pensée-islamique/',
     'https://www.devoirat.net/pensée-islamique/devoirs-pensée-islamique/3ème-année/',
-    'https://www.devoirat.net/physique/',
-    'https://www.devoirat.net/physique/cours-physique-chimie/',
     'https://www.devoirat.net/physique/cours-physique-chimie/4ème-année-bac/',
     'https://www.devoirat.net/physique/devoirs-physique/1ère-année/',
     'https://www.devoirat.net/physique/devoirs-physique/2ème-trimestre/',
-    'https://www.devoirat.net/physique/devoirs-physique/3ème-année/',
     'https://www.devoirat.net/physique/devoirs-physique/3ème-trimestre-1/',
-    'https://www.devoirat.net/physique/devoirs-physique/4ème-année-bac/',
     'https://www.devoirat.net/physique/devoirs-physique/bac-technique/',
     'https://www.devoirat.net/physique/devoirs-physique/sciences-exp/',
     'https://www.devoirat.net/physique/séries-physique-chimie/1ère-année/',
@@ -64,12 +47,8 @@ URLS = [
     'https://www.devoirat.net/physique/séries-physique-chimie/2ème-année/',
     'https://www.devoirat.net/physique/séries-physique-chimie/3ème-année/',
     'https://www.devoirat.net/physique/séries-physique-chimie/4ème-année-bac/',
-    'https://www.devoirat.net/sciences-svt/',
-    'https://www.devoirat.net/sciences-svt/cours-sciences-svt/',
     'https://www.devoirat.net/sciences-svt/séries-sciences-svt/1ère-année/',
     'https://www.devoirat.net/sciences-svt/séries-sciences-svt/4ème-année-bac/',
-    'https://www.devoirat.net/technologie/',
-    'https://www.devoirat.net/technologie/cours-technologie/',
     'https://www.devoirat.net/technologie/cours-technologie/2ème-année/',
     'https://www.devoirat.net/technologie/cours-technologie/3ème-année/',
     'https://www.devoirat.net/technologie/cours-technologie/4ème-année-bac/',
@@ -77,6 +56,54 @@ URLS = [
     'https://www.devoirat.net/technologie/devoirs-technologie/3ème-année/',
     'https://www.devoirat.net/technologie/séries-technologie/1ère-année/',
     'https://www.devoirat.net/économie-gestion-1/devoirs-economie-gestion/2ème-année/',
-    'https://www.devoirat.net/économie-gestion-1/séries-economie-gestion-1/',
     'https://www.devoirat.net/économie-gestion-1/séries-economie-gestion-1/3ème-economie-gestion/',
 ]
+
+def get_subject(url):
+    try:
+        url = url[len('https://www.devoirat.net/'):]
+        subject = url[:url.index('/')]
+        if subject[-2:] == '-1': subject = subject[:-2]
+
+        return subject
+    except Exception as e:
+        return None
+
+
+def get_category(url):
+    try:
+        categories = ['devoirs', 'cours', 'séries']
+        url = url[len('https://www.devoirat.net/'):]
+
+        for category in categories:
+            if category in url:
+                return category
+
+    except Exception as e:
+        pass
+
+    return None
+
+
+def get_level(url):
+    try:
+        levels = ['1ère', '2ème', '3ème', 'bac']
+        url = url[len('https://www.devoirat.net/'):]
+
+        for level in levels:
+            if level in url:
+                return level
+
+    except Exception as e:
+        pass
+
+    return None
+
+
+# d = {}
+# for url in URLS:
+#     level = get_level(url)
+#     d[level] = d.get(level, None) + 1 if d.get(level, None) is not None else 1
+
+# for i, j in d.items():
+#     print(i, j)
--- scrape/management/commands/scrapedevoiratdata.py
+++ scrape/management/commands/scrapedevoiratdata.py
@@ -1,4 +1,5 @@
 import io
+import os
 import re
 import pikepdf
 import requests
@@ -6,7 +7,19 @@ from bs4 import BeautifulSoup
 from PyPDF2 import PdfFileReader
 
 from django.core.management.base import BaseCommand
-from .devoirat_main_urls import URLS
+from .devoirat_main_urls import (
+    URLS,
+    get_subject,
+    get_level,
+    get_category,
+)
+from pdfs.models import (
+    PDF,
+    Subject,
+    Level,
+    Category,
+    Section
+)
 
 devoirat_url = 'https://www.devoirat.net/'
 pdf_link_re = r'^.+\.([pP][dD][fF])(.*)$'
@@ -28,9 +41,20 @@ class Command(BaseCommand):
             response = requests.get(url)
             soup = BeautifulSoup(response.content, 'html.parser')
 
-            self.extract_pdfs(soup)
+            # getting section name
+            # document.getElementsByTagName('h1')[1].textContent.split(':')[0].includes('Section')
+            section_name = None
+            for h1 in soup.find_all('h1'):
+                contents = h1.contents
+                if contents:
+                    content = contents[0]
+                    splitted = content.split(':')
+                    if 'section' in splitted[0].strip().lower():
+                        section_name = splitted[1].strip()
 
-    def extract_pdfs(self, soup):
+            self.extract_pdfs(soup, url, section_name)
+
+    def extract_pdfs(self, soup, url, section_name):
         """
         Extracting all PDFs from a web page
 
@@ -39,40 +63,99 @@ class Command(BaseCommand):
         for a in soup.find_all('a', {"class": "cc-m-download-link"}, href=True):
             link_content= a['href']
 
-            if re.match(pdf_link_re, link_content):
-                pdf_href = f"{devoirat_url}{a['href']}"
-                response = requests.get(pdf_href)
+            if not re.match(pdf_link_re, link_content):
+                continue
 
-                print ("url match: ", pdf_href)
+            pdf_href = f"{devoirat_url}{a['href']}"
+            response = requests.get(pdf_href)
 
-                parent_div = a.parent.parent
+            parent_div = a.parent.parent
 
-                title = parent_div.parent.find("div", {"class": "cc-m-download-title"})
-                description = parent_div.parent.find("div", {"class": "cc-m-download-description"})
-                file_name = parent_div.find("div", {"class": "cc-m-download-file-name"})
-                file_info = parent_div.find("div", {"class": "cc-m-download-file-info"})
-                file_type = file_info.find("span", {"class": "cc-m-download-file-type"}) if file_info else None
-                file_size = file_info.find("span", {"class": "cc-m-download-file-size"}) if file_info else None
+            title_div = parent_div.parent.find("div", {"class": "cc-m-download-title"})
+            description_div = parent_div.parent.find("div", {"class": "cc-m-download-description"})
+            file_name_div = parent_div.find("div", {"class": "cc-m-download-file-name"})
+            file_info_div = parent_div.find("div", {"class": "cc-m-download-file-info"})
+            file_type_div = file_info_div.find("span", {"class": "cc-m-download-file-type"}) if file_info_div else None
+            file_size_div = file_info_div.find("span", {"class": "cc-m-download-file-size"}) if file_info_div else None
 
-                print ("File Data:")
+            title = title_div.contents[0] if title_div else None
+            description = description_div.contents[0] if description_div else None
+            file_name = file_name_div.contents[0] if file_name_div else None
+            file_type = file_type_div.contents[0] if file_type_div else None
+            file_size = file_size_div.contents[0] if file_size_div else None
+            
+            # print ("File Data:")
 
-                # print("%-15s%-15s%-15s" % ('', 'title:', title.contents[0] if title else None))
-                print("%-15s%-15s%-15s" % ('', 'file name:', file_name.contents[0] if file_name else None))
-                # print("%-15s%-15s%-15s" % ('', 'description:', description.contents[0] if description else None))
-                # print("%-15s%-15s%-15s" % ('', 'file type:', file_type.contents[0] if file_type else None))
-                print("%-15s%-15s%-15s" % ('', 'file size:', file_size.contents[0] if file_size else None))
+            # print("%-15s%-15s%-15s" % ('', 'title:', title))
+            # print("%-15s%-15s%-15s" % ('', 'file name:', description))
+            # print("%-15s%-15s%-15s" % ('', 'description:', file_name))
+            # print("%-15s%-15s%-15s" % ('', 'file type:', file_type))
+            # print("%-15s%-15s%-15s" % ('', 'file size:', file_size))
 
-                with io.BytesIO(response.content) as f:
-                    # pdf = PdfFileReader(f)
+            dc_creator = None
+            dc_title = None
+            dc_description = None
+            dc_subject = None
+            with io.BytesIO(response.content) as f:
+                # pdf = PdfFileReader(f)
 
-                    pdf = pikepdf.open(f)
-                    # print()
-                    print("%-15s%-15s%-15s" % ('', 'pages count', len(pdf.pages)))
+                pdfIO = pikepdf.open(f)
+                # print()
+                # print("%-15s%-15s%-15s" % ('', 'pages count', len(pdfIO.pages)))
 
-                    meta_data = pdf.open_metadata()
-                    # print("Meta Data:")
-                    # print("%-15s%-15s%-15s" % ('', 'dc:creator', meta_data.get('dc:creator')))
-                    # print("%-15s%-15s%-15s" % ('', 'dc:title', meta_data.get('dc:title')))
-                    # print("%-15s%-15s%-15s" % ('', 'dc:description', meta_data.get('dc:description')))
-                    # print("%-15s%-15s%-15s" % ('', 'dc:subject', meta_data.get('dc:subject')))
-                    print()
+                meta_data = pdfIO.open_metadata()
+                dc_creator = meta_data.get('dc:creator')
+                dc_title = meta_data.get('dc:title')
+                dc_description = meta_data.get('dc:description')
+                dc_subject = meta_data.get('dc:subject')
+
+                # print("Meta Data:")
+                # print("%-15s%-15s%-15s" % ('', 'dc:creator', dc_creator))
+                # print("%-15s%-15s%-15s" % ('', 'dc:title', dc_title))
+                # print("%-15s%-15s%-15s" % ('', 'dc:description', dc_description))
+                # print("%-15s%-15s%-15s" % ('', 'dc:subject', dc_subject))
+                print()
+
+            pdf_instance = PDF(
+                title=title,
+                description=description,
+                name=file_name,
+                slug=file_name.replace(' ', '-').replace('--', '-'),
+                # type=file_type,
+                size=file_size,
+                dc_creator=dc_creator,
+                dc_title=dc_title,
+                dc_description=dc_description,
+                dc_subject=dc_subject,
+            )
+
+            temporarylocation="file.pdf"
+            with open(temporarylocation,'wb') as out:
+                out.write(io.BytesIO(response.content).read())
+
+            with open(temporarylocation, 'rb') as read:
+                pdf_instance.file.save(file_name, read)
+
+            os.remove(temporarylocation) # Delete file when done
+            
+            subject_name = get_subject(url)
+            if subject_name:
+                subject, created = Subject.objects.get_or_create(name=subject_name)
+                pdf_instance.subject = subject
+
+            level_name = get_level(url)
+            if level_name:
+                level, created = Level.objects.get_or_create(name=level_name)
+                pdf_instance.level = level
+
+            category_name = get_category(url)
+            if category_name:
+                category, created = Category.objects.get_or_create(name=category_name)
+                pdf_instance.category = category
+
+            if section_name:
+                section, created = Section.objects.get_or_create(name=section_name)
+                pdf_instance.section = section
+
+            pdf_instance.save()
+            print('     %-50s%-30s%-15s%-10s%-10s%-10s' % (file_name, description, subject_name, section_name, level_name, category_name))
