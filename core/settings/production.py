5a4842584f482605582c6cfc1454930a9f4567e5
A-Arari <araariadem0@gmail.com>
2020-11-19 21:36:00 +0100

    add production configs

--- core/settings/production.py
+++ core/settings/production.py
@@ -7,10 +7,24 @@ SECRET_KEY = 'Empty'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['IP-address or domain-name']
+ALLOWED_HOSTS = ['185.143.45.139', '0.0.0.0', 'server.exercice.net']
 
 
 # DB Configs missing
+
+# Database
+# https://docs.djangoproject.com/en/3.1/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'exercice',
+        'USER': 'exercice_user',
+        'PASSWORD': 'mode12az12',
+        'HOST': 'localhost',
+        'PORT': '',
+    }
+}
 
 
 # Password validation
@@ -30,4 +44,18 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
+
+
+# Internationalization
+# https://docs.djangoproject.com/en/3.1/topics/i18n/
+
+LANGUAGE_CODE = 'en-us'
+
+TIME_ZONE = 'UTC'
+
+USE_I18N = True
+
+USE_L10N = True
+
+USE_TZ = True
 
