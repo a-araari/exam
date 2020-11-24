569f10d8ef02cfabd8b5f0ad5d0187c4bf5d8257
A-Arari <araariadem0@gmail.com>
2020-11-24 16:33:11 +0100

    Run pdf2htmlEX with '--no-drm 1' argument on linux systems

--- pdfs/management/commands/_pdf_to_html_converter.py
+++ pdfs/management/commands/_pdf_to_html_converter.py
@@ -42,7 +42,7 @@ elif platform in ('linux', 'linux2', 'ubuntu'):
         pdf_location = str(pdf_location).replace(str(settings.BASE_DIR)+'/', '')
         temp_file_name = 'file.html'
         FNULL = open(os.devnull, 'w')
-        print('Running:', *['docker', 'run', '-ti', '--rm', '-v', str(settings.BASE_DIR) + ':/pdf', 'bwits/pdf2htmlex', 'pdf2htmlEX', '--zoom', '1.3', pdf_location, temp_file_name])
+        print('Running:', *['docker', 'run', '-ti', '--rm', '-v', str(settings.BASE_DIR) + ':/pdf', 'bwits/pdf2htmlex', 'pdf2htmlEX', '--zoom', '1.3', '--no-drm', '1', pdf_location, temp_file_name])
         response = str(
             subprocess.run(
                 ['docker', 'run', '-ti', '--rm', '-v', str(settings.BASE_DIR) + ':/pdf', 'bwits/pdf2htmlex', 'pdf2htmlEX', '--zoom', '1.3', pdf_location, temp_file_name],
