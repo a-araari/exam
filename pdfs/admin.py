f1a993d05a553d73123ca7fab3aa7c3023b9bba2
A-Arari <araariadem0@gmail.com>
2020-11-24 01:36:43 +0100

    Reorder list_filter

--- pdfs/admin.py
+++ pdfs/admin.py
@@ -13,27 +13,47 @@ from pdfs.models import (
 @admin.register(PDF)
 class PDFAdmin(admin.ModelAdmin):
     list_display = ('title', 'description', 'subject', 'section', 'level', 'category')
-    list_filter = ('stage', 'subject', 'section', 'level', 'category')
+    list_filter = ('stage', 'category', 'section', 'level', 'subject')
 
 
 @admin.register(Subject)
 class SubjectAdmin(admin.ModelAdmin):
-    list_display = ('name',)
+    list_display = ('name', 'get_pdfs_count')
+
+    def get_pdfs_count(self, instance, *args, **kwargs):
+        return PDF.objects.filter(subject=instance).count()
+
+    get_pdfs_count.short_description = 'related pdfs count'
 
 
 @admin.register(Section)
 class SectionAdmin(admin.ModelAdmin):
-    list_display = ('name',)
+    list_display = ('name', 'get_pdfs_count')
+
+    def get_pdfs_count(self, instance, *args, **kwargs):
+        return PDF.objects.filter(section=instance).count()
+
+    get_pdfs_count.short_description = 'related pdfs count'
 
 
 @admin.register(Level)
 class LevelAdmin(admin.ModelAdmin):
-    list_display = ('name',)
+    list_display = ('name', 'get_pdfs_count')
+
+    def get_pdfs_count(self, instance, *args, **kwargs):
+        return PDF.objects.filter(level=instance).count()
+
+    get_pdfs_count.short_description = 'related pdfs count'
 
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
-    list_display = ('name',)
+    list_display = ('name', 'get_pdfs_count')
+
+    def get_pdfs_count(self, instance, *args, **kwargs):
+        return PDF.objects.filter(category=instance).count()
+        
+    get_pdfs_count.short_description = 'related pdfs count'
 
 
 @admin.register(PDFError)
