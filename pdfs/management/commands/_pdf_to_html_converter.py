b28ddbb73ed842c24617d6d494bc60dc2e856bf0
A-Arari <araariadem0@gmail.com>
2020-11-29 18:43:28 +0100

    Remove double quotes from files' path

--- pdfs/management/commands/_pdf_to_html_converter.py
+++ pdfs/management/commands/_pdf_to_html_converter.py
@@ -43,8 +43,7 @@ elif platform in ('linux', 'linux2', 'ubuntu'):
         print()
         print(f'ID={id}')
         pdf_location = str(pdf_location).replace(str(settings.BASE_DIR)+'/', '')
-        pdf_location = f'"{pdf_location}"'
-        temp_file_name = '"file.html"'
+        temp_file_name = 'file.html'
         print('Running:', *['docker', 'run', '-ti', '--rm', '-v', str(settings.BASE_DIR) + ':/pdf', 'bwits/pdf2htmlex', 'pdf2htmlEX', '--zoom', '1.3', '--no-drm', '1', pdf_location, temp_file_name])
         response = str(
             subprocess.run(
