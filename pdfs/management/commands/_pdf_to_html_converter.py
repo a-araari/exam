fec90e22a049ed7b4551807ddd09ac9c0ad16a04
A-Arari <araariadem0@gmail.com>
2020-11-29 03:16:42 +0100

    Fix minor erro

--- pdfs/management/commands/_pdf_to_html_converter.py
+++ pdfs/management/commands/_pdf_to_html_converter.py
@@ -45,6 +45,7 @@ elif platform in ('linux', 'linux2', 'ubuntu'):
         pdf_location = str(pdf_location).replace(str(settings.BASE_DIR)+'/', '')
         pdf_location = f'"{pdf_location}"'
         temp_file_name = '"file.html"'
+        print('Running:', *['docker', 'run', '-ti', '--rm', '-v', str(settings.BASE_DIR) + ':/pdf', 'bwits/pdf2htmlex', 'pdf2htmlEX', '--zoom', '1.3', '--no-drm', '1', pdf_location, temp_file_name])
         response = str(
             subprocess.run(
                 ['docker', 'run', '-ti', '--rm', '-v', str(settings.BASE_DIR) + ':/pdf', 'bwits/pdf2htmlex', 'pdf2htmlEX', '--zoom', '1.3', '--no-drm', '1', pdf_location, temp_file_name]
@@ -52,7 +53,6 @@ elif platform in ('linux', 'linux2', 'ubuntu'):
         )
 
         if 'returncode=0' not in response.lower():
-            print('NONE returned --------------')
             return None
 
         return temp_file_name
