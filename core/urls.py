80b2dd1f0fbbb46de1db24e091b5c91449c1a465
A-Arari <araariadem0@gmail.com>
2020-12-11 01:30:45 +0100

    Set pdfs.urls at the end of urlpatterns

--- core/urls.py
+++ core/urls.py
@@ -21,8 +21,8 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('', include('home.urls')),
-    path('', include('pdfs.urls')),
     path('admin/', admin.site.urls),
+    path('', include('pdfs.urls')),
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
