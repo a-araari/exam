4f82a89089a3cc0e6b03845c8263738ad52e57c4
A-Arari <araariadem0@gmail.com>
2020-12-17 02:41:30 +0100

    Add last two sections to the landing page

--- static_files/js/base.js
+++ static_files/js/base.js
@@ -11,4 +11,26 @@ $(document).ready(function() {
         else if (navbarExpandIcon.hasClass('fa-caret-down'))
             navbarExpandIcon.addClass('fa-caret-up').removeClass('fa-caret-down')
     })
+    $('#recipeCarousel').carousel({
+        interval: 10000
+    })
+
+    $('.carousel .carousel-item').each(function(){
+        var minPerSlide = 3;
+        var next = $(this).next();
+        if (!next.length) {
+            next = $(this).siblings(':first');
+        }
+        next.children(':first-child').clone().appendTo($(this));
+        
+        for (var i=0;i<minPerSlide;i++) {
+            next=next.next();
+            if (!next.length) {
+                next = $(this).siblings(':first');
+            }
+            
+            next.children(':first-child').clone().appendTo($(this));
+          }
+    });
+
 });
