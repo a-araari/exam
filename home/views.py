c2b7e0e9fb7b1227005627df05719ae7f0946a20
A-Arari <araariadem0@gmail.com>
2020-12-27 02:06:06 +0100

    Add the search functionality

--- home/views.py
+++ home/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
 from django.utils.translation import gettext as _
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from pdfs.models import (
+    Level,
+    PDF,
+)
 
 
 def index(request):
@@ -11,17 +16,35 @@ def index(request):
 
 
 def search(request):
-    q = None
-    title = _('Searching for exams, series and courses')
+    pdfs = None
+    q = request.GET.get('q')
+    page = request.GET.get('page', 1)
+    max_pdfs_per_page = request.GET.get('docs_per_page', 8)
 
-    if request.method == 'GET':
-        q = request.GET.get('q')
-        if q:
-            print(q)
-            title = _('Searching for: ') + q
+    title = _('Searching for exams, series and courses')
+    high_school_stage_levels = Level.objects.get_high_school_stage_levels()
+    middle_school_stage_levels = Level.objects.get_middle_school_stage_levels()
+    elementary_school_stage_levels = Level.objects.get_elementary_school_stage_levels()
+
+    if q:
+        title = _('Searching for: ') + q
+        pdfs = PDF.objects.filter(name__contains=q)
+    
+        paginator = Paginator(pdfs, max_pdfs_per_page)
+        try:
+            pdfs = paginator.page(page)
+        except PageNotAnInteger:
+            pdfs = paginator.page(1)
+        except EmptyPage:
+            pdfs = paginator.page(paginator.num_pages)
 
     context = {
+        'high_school_stage_levels': high_school_stage_levels,
+        'middle_school_stage_levels': middle_school_stage_levels,
+        'elementary_school_stage_levels': elementary_school_stage_levels,
         'title': title,
+        'pdfs': pdfs,
+        'query': q,
     }
 
     return render(request, 'home/search.html', context)
